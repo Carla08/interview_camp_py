3e6c431a68caa10de67e464baf7e7608992f81f9
Carla Prieto <carla.prieto@spectrumeffect.com>
2019-03-31 15:33:17 -0600

    binary search closest

--- week1/binary_search/binary_search.py
+++ week1/binary_search/binary_search.py
@@ -37,3 +37,27 @@ def binary_search_duplicates(arr, element):
             return mid
 
     return None
+
+
+def binary_search_closest(arr, number):
+    # NOTE: arr should be sorted
+    start = 0
+    end = len(arr) - 1
+    closest = 100000000  # hardcoded huge value to avoid check for None on first iter.
+
+    while start <= end:
+        mid = start + (end - start) // 2  # to avoid overflow
+        element = arr[mid]
+        closest = mid if abs(number-element) < closest else closest
+        if element < number:
+            start = mid + 1
+            continue
+        elif element > number:
+            end = mid - 1
+        else:
+            # keep searching backwards until finding the first occurrence.
+            while arr[mid - 1] == number:
+                mid -= 1
+            return mid
+
+    return closest
--- week1/unit_tests/test_binary_search.py
+++ week1/unit_tests/test_binary_search.py
@@ -1,6 +1,7 @@
 import unittest
 from assertpy import assert_that
-from week1.binary_search.binary_search import binary_search, binary_search_duplicates
+from week1.binary_search.binary_search import binary_search, binary_search_duplicates, \
+    binary_search_closest
 
 class TestBinarySearch(unittest.TestCase):
     def test_binary_search(self):
@@ -14,3 +15,9 @@ class TestBinarySearch(unittest.TestCase):
         element = 2
         index_found = binary_search_duplicates(arr, element)
         assert_that(index_found).is_equal_to(1)
+
+    def test_binary_search_closest(self):
+        arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130]
+        number = 23
+        index_closest = binary_search_closest(arr, number)
+        assert_that(index_closest).is_equal_to(1)
