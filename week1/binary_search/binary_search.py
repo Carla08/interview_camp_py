1986089e65cf4e13087687557af67b824c25c62b
Carla Prieto <carla.prieto@spectrumeffect.com>
2019-03-31 17:08:18 -0600

    binary search cycled and binary search unkown length

--- week1/binary_search/binary_search.py
+++ week1/binary_search/binary_search.py
@@ -61,3 +61,56 @@ def binary_search_closest(arr, number):
             return mid
 
     return closest
+
+
+def find_min_in_cycled_arr(arr):
+    # NOTE: arr may be cycled. but sorted.
+    start = 0
+    end = len(arr) - 1
+    right = arr[end]
+
+    while start <= end:
+        mid = start + (end - start) // 2  # to avoid overflow
+        if arr[mid] < right and (mid == 0 or arr[mid -1] > arr[mid]):
+            return mid
+        elif arr[mid] > right:
+            start = mid + 1
+        elif arr[mid] < right:
+            end = mid - 1
+
+    return mid
+
+
+def binary_search_unknown_length(arr, element):
+    # NOTE: arr should be sorted
+    # do binary search for finding end.
+    start, end = 0, 1
+    end_found, limit = False, None
+    while start < end:
+        try:
+            arr[end]
+            start = end
+            if not end_found:
+                end *= 2
+            else:
+                end = (end + limit) // 2
+        except IndexError:
+            # end should be between prev and possible_end
+            end_found = True
+            limit = end
+            end =(start + end) // 2
+
+    # find the element
+    start = 0
+    while start <= end:
+        mid = start + (end - start) // 2  # to avoid overflow
+        if arr[mid] < element:
+            start = mid + 1
+            continue
+        elif arr[mid] > element:
+            end = mid - 1
+        else:
+            # arr[mid] = element. Found it!
+            return mid
+
+    return None
--- week1/unit_tests/test_binary_search.py
+++ week1/unit_tests/test_binary_search.py
@@ -1,7 +1,7 @@
 import unittest
 from assertpy import assert_that
 from week1.binary_search.binary_search import binary_search, binary_search_duplicates, \
-    binary_search_closest
+    binary_search_closest, find_min_in_cycled_arr, binary_search_unknown_length
 
 class TestBinarySearch(unittest.TestCase):
     def test_binary_search(self):
@@ -21,3 +21,14 @@ class TestBinarySearch(unittest.TestCase):
         number = 23
         index_closest = binary_search_closest(arr, number)
         assert_that(index_closest).is_equal_to(1)
+
+    def test_find_min_cycled_sorted_arr(self):
+        arr = [9,8,7,6,5,1,2]
+        index_min = find_min_in_cycled_arr(arr)
+        assert_that(index_min).is_equal_to(5)
+
+    def test_binary_search_unknown_end(self):
+        arr = [1,2,3,4,5,6,7,8,9,10]
+        element = 2
+        index_found = binary_search_unknown_length(arr, element)
+        assert_that(index_found).is_equal_to(1)
